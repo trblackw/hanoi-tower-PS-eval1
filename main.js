61f55e952160431c57e9561af41b50b51a5ae347
Tucker <trblackw@ncsu.edu>
2018-11-02 12:37:39 -0400

    implemented winning run and win logic

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const discSet1 = document.querySelector("#disc-1");
 const discSet2 = document.querySelector("#disc-2");
 const discSet3 = document.querySelector("#disc-3");
+const submitButton = document.querySelector("button");
+const startPegInput = document.querySelector("input[name=start]");
+const endPegInput = document.querySelector("input[name=end]");
 
 var Board = function() {
   var set = [[5, 4, 3, 2, 1], [], []];
@@ -25,7 +28,6 @@ var Board = function() {
         }
       });
     }
-    //  console.log(availableMoves);
     return availableMoves;
   };
 
@@ -36,7 +38,7 @@ var Board = function() {
     };
     //prints board vertically
     var vertical = function() {
-      return set[0].map((x, row) => set.map(y => y[row]));
+      return set[0].map((x, row) => set.map(y => x[row]));
     };
 
     return {
@@ -50,22 +52,32 @@ var Board = function() {
     if (startPeg > 3 || (endPeg > 3 && (startPeg > 0 && endPeg > 0))) {
       return "invalid move!";
     }
-    var startPeg = set[startPeg - 1];
-    var endPeg = set[endPeg - 1];
+    var pegToMove = set[startPeg - 1];
+    var pegToReceive = set[endPeg - 1];
+
+    //  var complete =
+    //    set.filter(peg => peg !== pegToReceive).filter(arr => arr.length === 0)
+    //      .length === 2;
 
     //check to make sure disc to stack on top is smaller than the disc below it
-    if (endPeg[endPeg.length - 1] < startPeg[startPeg.length - 1]) {
-      return "invalid move, the disc you're trying to stack on top is larger than the one below it";
-    } else if (endPeg.length === 5) {
-      //  endPeg.reduce((winningArr, disc, i) => {
-
-      //  })
-      console.log("you win!");
+    if (
+      pegToReceive[pegToReceive.length - 1] < pegToMove[pegToMove.length - 1]
+    ) {
+      alert(
+        "invalid move, the disc you're trying to stack on top is larger than the one below it"
+      );
+    } else if (complete) {
+      alert(`Congrats! You won in ${moves.count} moves`);
     } else {
-      var discToMove = startPeg.pop();
-      endPeg.push(discToMove);
+      var discToMove = pegToMove.pop();
+      pegToReceive.push(discToMove);
       incrementMoves();
-      console.log(set);
+      var complete =
+        set.filter(peg => peg !== pegToReceive).filter(arr => arr.length === 0)
+          .length === 2;
+      return complete
+        ? alert(`Congrats! You won in ${moves.count} moves`)
+        : null;
       console.log(moves);
     }
   };
@@ -83,13 +95,53 @@ var Board = function() {
 
 var board = Board();
 
+// submitButton.addEventListener("click", () => {
+//   board.move(startPegInput.value, endPegInput.value);
+//   const discs = [...document.querySelectorAll(".discs")];
+//   // console.log(discs[endPegInput.value - 1]);
+//   discs[endPegInput.value - 1].innerHTML = board.stats.set[
+//     endPegInput.value - 1
+//   ]
+//     .map(disc => `<span>${disc}</span>`)
+//     .join("");
+
+//   (startPegInput.value = ""), (endPegInput.value = "");
+// });
+
 var [firstPeg, secondPeg, thirdPeg] = board.stats.set;
 
+//winning run
 board.move(1, 2);
 board.move(1, 3);
-// board.move(2, 3);
-// board.move(1, 2);
-// board.move(3, 2);
+board.move(2, 3);
+board.move(1, 2);
+board.move(3, 1);
+board.move(3, 2);
+board.move(1, 2);
+board.move(1, 3);
+board.move(2, 3);
+board.move(2, 1);
+board.move(3, 1);
+board.move(2, 3);
+board.move(1, 2);
+board.move(1, 3);
+board.move(2, 3);
+board.move(1, 2);
+board.move(3, 1);
+board.move(3, 2);
+board.move(1, 2);
+board.move(3, 1);
+board.move(2, 3);
+board.move(2, 1);
+board.move(3, 1);
+board.move(3, 2);
+board.move(1, 2);
+board.move(1, 3);
+board.move(2, 3);
+board.move(1, 2);
+board.move(3, 1);
+board.move(3, 2);
+board.move(1, 2);
 
 //make copies to not mutate state
 var firstPegCopy = firstPeg.slice(0);
